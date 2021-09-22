792afaf1cbecfa075a964f0054e5d15b25a8138d
marymw <71140577+marymw@users.noreply.github.com>
2021-09-22 22:14:56 +0300

    Create Lifo.h

---
+++ Lifo.h
@@ -0,0 +1,16 @@
+#include <string.h>
+
+const int POISON        = 0xF0;
+const int FREED_POINTER =   13;
+
+struct Stack{
+	int   *StackData;//указатель на начало данных
+	size_t StackSize;//текущий размер стека
+	size_t StackCapacity; //текущая выделенная память под стек
+};
+
+int StackCtor  (Stack *FirstStackPtr);
+int StackDtor  (Stack *FirstStackPtr);
+int StackMemory(Stack *FirstStackPtr);
+int StackPush  (Stack *FirstStackPtr, int Value);
+int StackPop   (Stack *FirstStackPtr, int *PtrOnValue);
