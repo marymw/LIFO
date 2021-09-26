0c031b65ac656320f9b48a6ae7b4c0bbce3f99e8
marymw <71140577+marymw@users.noreply.github.com>
2021-09-26 14:18:47 +0300

    Update Lifo.h

--- Lifo.h
+++ Lifo.h
@@ -1,4 +1,18 @@
-include <string.h>
+#include <string.h>
+
+#define DEBUG 1
+
+#ifdef DEBUG
+#define DEBUG_PRINTF(string) printf(string)
+#define DEBUG_PRINTF_ONE_ARG(format,var) printf(format,var)
+#else
+#define DEBUG_PRINTF(string) printf("\n");
+#endif
+
+enum Errors {
+	NO_ERRORS,      //0
+	STK_UNDEFINED   //1
+};
 
 const int POISON        = 0xF0;
 const int FREED_POINTER =   13;
@@ -13,4 +27,4 @@ int StackCtor  (Stack *FirstStackPtr);
 int StackDtor  (Stack *FirstStackPtr);
 int StackMemory(Stack *FirstStackPtr);
 int StackPush  (Stack *FirstStackPtr, int Value);
-int StackPop   (Stack *FirstStackPtr, int *PtrOnValue);
+int StackPop   (Stack *SomeStackPtr, int *StatusStackPop = NULL);
