f64ea6d30dc64e63212fc67d94e8335e6dc57a86
marymw <71140577+marymw@users.noreply.github.com>
2021-09-26 14:59:39 +0300

    Update MainLifo.cpp

--- MainLifo.cpp
+++ MainLifo.cpp
@@ -8,24 +8,38 @@ int main() {
 	int StatusStackCtor = StackCtor(&FirstStack); //конструктор стека, создает стек, возвращает ошибку
 	assert(StatusStackCtor == 0);
 
+	StackPrint(FirstStack);
+
+
 	int StatusStackPush = StackPush(&FirstStack, 10);//кладет что-то в конец стека
 	assert(StatusStackPush == 0);
+
+	StackPrint(FirstStack);
+
 
 	StatusStackPush = StackPush(&FirstStack, 20);//кладет что-то в конец стека
 	assert(StatusStackPush == 0);
 
+	StackPrint(FirstStack);
+
+
 	int StatusStackPop = 0;
+
+	StackPrint(FirstStack);
 
 	int ValueFromPop1 = StackPop(&FirstStack, &StatusStackPop); //вытаскивает один элемент из стека
 	assert(StatusStackPop == 0);
 
 	printf("%d\n", ValueFromPop1);
 
+	StackPrint(FirstStack);
+
 	int ValueFromPop2 = StackPop(&FirstStack); //вытаскивает один элемент из стека
 	assert(StatusStackPop == 0);
 
 	printf("%d\n", ValueFromPop2);
 
+	StackPrint(FirstStack);
 
 	int StatusStackDtor = StackDtor(&FirstStack);//деструктор стека
 	assert(StatusStackDtor == 0);
