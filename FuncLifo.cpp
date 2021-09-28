de0e30e77fe1395b86e60b1a2bda51a661f64223
marymw <71140577+marymw@users.noreply.github.com>
2021-09-28 23:00:22 +0300

    Update FuncLifo.cpp

--- FuncLifo.cpp
+++ FuncLifo.cpp
@@ -5,170 +5,141 @@
 #include <string.h>
 #include <stdio.h>
 
-//STACKRESIZE
 //type
 
-int StackCtor(Stack *SomeStackPtr){//конструктор стека, создает стек, возвращает ошибку
-	assert(SomeStackPtr); //проверили, существует ли вообще то что нам передали
+int StackCtor(Stack *someStackPtr){
+	assert(someStackPtr); 
 
-	int StatusStackCtor = 0; //если никаких ошибок
-	SomeStackPtr->StackCapacity = 10; //пусть в начале выделяется на 10, а там посмотрим
+	int statusStackCtor = 0; 
+	someStackPtr->stackCapacity = 10; //пусть в начале выделяется на 10, а там посмотрим
 
-	SomeStackPtr->StackData = (int *)calloc(SomeStackPtr->StackCapacity, sizeof(int));//какой размер передавать стеку 
-																						//в консрукторе
-
-	if (SomeStackPtr->StackData == nullptr){
-		StatusStackCtor = 1; //ошибка связанная с выделением памяти
-		return StatusStackCtor;
+	someStackPtr->stackData = (int *)calloc(someStackPtr->stackCapacity, sizeof(int));
+																						/
+	if (someStackPtr->stackData == nullptr){
+		statusStackCtor = 1; 
+		return statusStackCtor;
 	}
 
-	SomeStackPtr->StackSize = 0; //удалили данные
+	someStackPtr->stackSize = 0; //удалили данные
 	//надо какую-то проверку на size потом делать??
 
 	
-	return StatusStackCtor; //возвращает код ошибки
+	return statusStackCtor; //возвращает код ошибки
 }
 
 
-int StackPush(Stack *SomeStackPtr, int Value){//кладет что-то в конец стека
-	assert(SomeStackPtr); //проверили, существует ли вообще то что нам передали
+int StackPush(Stack *someStackPtr, int value){//кладет что-то в конец стека
+	assert(someStackPtr); //проверили, существует ли вообще то что нам передали
 
-	int StatusStackPush = 0; //если никаких ошибок
+	int statusStackPush = 0; //если никаких ошибок
 
-	int StatusStackMemory = StackResize(SomeStackPtr);//функция определяет сколько  памяти зарезервировать стеку
-	assert(StatusStackMemory == 0);
+	int statusStackMemory = StackResize(someStackPtr);//функция определяет сколько  памяти зарезервировать стеку
+	assert(statusStackMemory == 0);
 
-	SomeStackPtr->StackData[SomeStackPtr->StackSize] = Value;
-	SomeStackPtr->StackSize++;
+	someStackPtr->stackData[someStackPtr->stackSize] = value;
+	someStackPtr->stackSize++;
 	
-	return StatusStackPush;
+	return statusStackPush;
 }
 
 
-int StackPop(Stack *SomeStackPtr, int *StatusStackPop){//вытаскивает один элемент из стека
-	assert(SomeStackPtr);
+int StackPop(Stack *someStackPtr, int *statusStackPop){//вытаскивает один элемент из стека
+	assert(someStackPtr);
 
-	DEBUG_PRINTF_ONE_ARG("I am in beggining of function %s\n", __FUNCTION__);
-/* из-за этого куска всё падает
-	if (SomeStackPtr == NULL){ //проверили, существует ли вообще то что нам передали
-		DEBUG_PRINTF("в операторе if \n");
-		*StatusStackPop = STK_UNDEFINED;
-		//а как выйти в этом случае??
-	}
-	else{
-		*StatusStackPop = NO_ERRORS;
-		DEBUG_PRINTF("в операторе if \n");
-	}
-	//int StatusStackPop = 0; //если никаких ошибок
-*/
+	int statusStackMemory = StackResize(someStackPtr);//функция определяет сколько  памяти зарезервировать стеку
+	assert(statusStackMemory == 0);
 
+	someStackPtr->stackSize--;
+	int valueFromPop = someStackPtr->stackData[someStackPtr->stackSize];
 
-	DEBUG_PRINTF_ONE_ARG("I am in function %s\n", __FUNCTION__);
+	someStackPtr->stackData[someStackPtr->stackSize] = POISON;
 
-	int StatusStackMemory = StackResize(SomeStackPtr);//функция определяет сколько  памяти зарезервировать стеку
-	assert(StatusStackMemory == 0);
-
-	SomeStackPtr->StackSize--;
-	int ValueFromPop = SomeStackPtr->StackData[SomeStackPtr->StackSize];
-
-	SomeStackPtr->StackData[SomeStackPtr->StackSize] = POISON;
-
-	DEBUG_PRINTF_ONE_ARG("I am in the end function %s\n", __FUNCTION__);
-
-	return ValueFromPop;
+	return valueFromPop;
 }
 
 
-int StackDtor(Stack *SomeStackPtr){//деструктор стека
+int StackDtor(Stack *someStackPtr){//деструктор стека
 
-	assert(SomeStackPtr); //проверили, существует ли вообще то что нам передали
+	assert(someStackPtr); //проверили, существует ли вообще то что нам передали
+	//!TODO StackOK
 
-	int StatusStackDtor = 0; //если никаких ошибок
+	int statusStackDtor = 0; //если никаких ошибок
 
-	//залили ядом
-	memset(SomeStackPtr->StackData, POISON, SomeStackPtr->StackCapacity);//размер заполняемой области  StackCapacity
+	memset(someStackPtr->stackData, POISON, someStackPtr->stackCapacity);//размер заполняемой области  StackCapacity
 																		//или StackSize? почему именно 0xF0
 	//так как free не чистит память
 
 	
-	free(SomeStackPtr->StackData);
-	SomeStackPtr->StackData = (int *)FREED_POINTER;// 13 - маленьное число, маркер уже освобожденных укахателей 
+	free(someStackPtr->stackData);
+	someStackPtr->stackData = (int *)FREED_POINTER;// 13 - маленьное число, маркер уже освобожденных укахателей 
 
-	SomeStackPtr->StackSize = INT_MAX;//самое большое число size_t какое?
-	SomeStackPtr->StackCapacity = 0;//норм????
+	someStackPtr->stackSize = INT_MAX;//самое большое число size_t какое?
+	someStackPtr->stackCapacity = 0;//норм????
 
 
-	return StatusStackDtor;
+	return statusStackDtor;
 }
 
+//надо бы протестить как-то
+int StackResize (Stack *someStackPtr){//управлет размером памяти, выделенной под стек
 
-int StackResize (Stack *SomeStackPtr){//управлет размером памяти, выделенной под стек
-	int StatusStackMemory = 0; //если всё ок
-	//printf("Функция %s ещё не реализована\n хрум - хрум\n", __FUNCTION__);
+	int statusStackMemory = 0; //надо возвращать её
 
-	if (SomeStackPtr->StackCapacity >= LARGE_VOLUME_CRITERION * SomeStackPtr->StackSize){
-		int *ReallocDataPtr = (int *)realloc(SomeStackPtr->StackData, 
-												  SomeStackPtr->StackCapacity * SMALL_COEFFICIENT);
-		if (ReallocDataPtr == nullptr) {
+	int sizeOfSomeStack     = someStackPtr->stackSize;
+	int capacityOfSomeStack = someStackPtr->stackCapacity;
+	int *dataOfSomeStack    = someStackPtr->stackData;
+
+	if (capacityOfSomeStack >= LARGE_VOLUME_CRITERION * sizeOfSomeStack){
+		int *reallocDataPtr = (int *)realloc(dataOfSomeStack, capacityOfSomeStack * NORMAL_DECREASE_COEFF);
+		if (reallocDataPtr == nullptr) {
 			return LACK_OF_MEMORY;
 		}
 
-		SomeStackPtr->StackData = ReallocDataPtr;
-		SomeStackPtr->StackCapacity *= SMALL_COEFFICIENT;
+		someStackPtr->stackData      = reallocDataPtr;
+		someStackPtr->stackCapacity *= NORMAL_DECREASE_COEFF;
 	
 	}
-	else if (SomeStackPtr->StackCapacity <= SMALL_VOLUME_CRITERION * SomeStackPtr->StackSize){
-		if (SomeStackPtr->StackSize > VERY_BIG_SIZE_OF_STACK) {
+	 
+	else if (capacityOfSomeStack <= SMALL_VOLUME_CRITERION * sizeOfSomeStack){
 
-			int *ReallocDataPtr = (int *)realloc(SomeStackPtr->StackData, 
-													  SomeStackPtr->StackCapacity * (VERY_SMALL_COEFFICIENT + 1));
-			if (ReallocDataPtr == nullptr) {
+		if (sizeOfSomeStack > BIG_SIZE_OF_STACK) {
+
+			int *reallocDataPtr = (int *)realloc(dataOfSomeStack, capacityOfSomeStack * SMALL_INCREASE_COEFF);
+			if (reallocDataPtr == nullptr) {
 				return LACK_OF_MEMORY;
 			}
 
-			SomeStackPtr->StackData = ReallocDataPtr;
-			SomeStackPtr->StackCapacity *= VERY_SMALL_COEFFICIENT + 1;//X + X*A = X(A+1)
-		}
-
-		else if (SomeStackPtr->StackSize > BIG_SIZE_OF_STACK) {
-
-			int *ReallocDataPtr = (int *)realloc(SomeStackPtr->StackData, 
-													  SomeStackPtr->StackCapacity * (SMALL_COEFFICIENT + 1));
-			if (ReallocDataPtr == nullptr) {
-				return LACK_OF_MEMORY;
-			}
-
-			SomeStackPtr->StackData = ReallocDataPtr;
-			SomeStackPtr->StackCapacity *= SMALL_COEFFICIENT + 1;//X + X*A = X(A+1)
+			someStackPtr->stackData      = reallocDataPtr;
+			someStackPtr->stackCapacity *= SMALL_INCREASE_COEFF;
 		}
 
 		else {
-			int *ReallocDataPtr = (int *)realloc(SomeStackPtr->StackData, SomeStackPtr->StackCapacity);
+			int *reallocDataPtr = (int *)realloc(dataOfSomeStack, capacityOfSomeStack * NORMAL_INCREASE_COEFF);
 
-			if (ReallocDataPtr == nullptr) {
+			if (reallocDataPtr == nullptr) {
 				return LACK_OF_MEMORY;
 			}
-			SomeStackPtr->StackData = ReallocDataPtr;
-			SomeStackPtr->StackCapacity *= 2;//проверка если налл 
+			someStackPtr->stackData = reallocDataPtr;
+			someStackPtr->stackCapacity *= NORMAL_INCREASE_COEFF; 
 		}
 	}
 	
-	return StatusStackMemory;
+	return statusStackMemory;
 }
 
 
-void StackPrint(Stack SomeStack){
+void StackPrint(Stack someStack){
 	PrintSeparator();
 	printf("Printing stack...\n\n");
 
-	if (SomeStack.StackSize == 0) {
+	if (someStack.stackSize == 0) {
 		printf("Stack is empty! :)\n\n");
 		PrintSeparator();
 		return;
 	}
 
-	for (int i = 0; i < SomeStack.StackSize; i++){
-		printf("%d\n", SomeStack.StackData[i]);
+	for (int i = 0; i < someStack.stackSize; i++){
+		printf("%d\n", someStack.stackData[i]);
 	}
 
 	PrintSeparator();
