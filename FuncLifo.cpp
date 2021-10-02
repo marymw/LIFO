69bd26aeeb495a0b988bdbff52368df50cc3a93c
marymw <71140577+marymw@users.noreply.github.com>
2021-10-02 11:45:24 +0300

    Update FuncLifo.cpp

--- FuncLifo.cpp
+++ FuncLifo.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "Lifo.h"
 #include <assert.h>
 #include <stdlib.h>
@@ -6,17 +7,24 @@
 #include <stdio.h>
 #include <ctype.h>
 
-//type
-//норм
+//type сделлать через typedef и поменять везде на type
+//норм, но всё что ниже нужно переделать для произвольного стека
+//int StackCtor(Stack *someStackPtr, size_t sizeOfTypeOfElements)
+
+const Type POISON = 66;
+const int FREED_POINTER = 13;
+const size_t SIZE_T_MAX = -1;
+
 int StackCtor(Stack *someStackPtr){
 
 	if (someStackPtr == 0) {
 		return StackNotOK(someStackPtr, __LINE__, __FILE__, "StackCtor");//ну такое себе решение..
 	} 
 
-	someStackPtr->stackCapacity = 10; //пусть в начале выделяется на 10, а там посмотрим
-	someStackPtr->stackData     = (int *)calloc(someStackPtr->stackCapacity, sizeof(int));
-	memset(someStackPtr->stackData, POISON, someStackPtr->stackCapacity);//что не тааааааак
+	someStackPtr->stackCapacity		   = 10; //пусть в начале выделяется на 10, а там посмотрим
+	//someStackPtr->sizeOfTypeOfElements = sizeOfTypeOfElements;
+	someStackPtr->stackData     	   = (Type *)calloc(someStackPtr->stackCapacity, sizeof(int));
+	memset(someStackPtr->stackData, 0xF0, someStackPtr->stackCapacity);//нельзя???
 																						
 	if (someStackPtr->stackData == nullptr){ 
 		return StackNotOK(someStackPtr, __LINE__, __FILE__, "StackCtor");
@@ -30,7 +38,7 @@ int StackCtor(Stack *someStackPtr){
 }
 
 //норм
-int StackPush(Stack *someStackPtr, const int value){ 
+int StackPush(Stack *someStackPtr, const Type value){ 
 
 	ASSERT_OK(someStackPtr);
 
@@ -46,17 +54,17 @@ int StackPush(Stack *someStackPtr, const int value){
 }
 
 //норм
-int StackPop(Stack *someStackPtr, int *statusStackPop){
+Type StackPop(Stack *someStackPtr, int *statusStackPop){
 
 	ASSERT_OK(someStackPtr);
 
 	int statusStackMemory = StackResize(someStackPtr);
-	assert(statusStackMemory == 0);
+	assert(statusStackMemory == 0);//в лог, не падать
 
 	someStackPtr->stackSize--;
-	int sizeOfSomeStack = someStackPtr->stackSize;//int sizeOfSomeStack = (--someStackPtr->stackSize);
+	size_t sizeOfSomeStack = someStackPtr->stackSize;//int sizeOfSomeStack = (--someStackPtr->stackSize);
 
-	int valueFromPop = someStackPtr->stackData[sizeOfSomeStack];
+	Type valueFromPop = someStackPtr->stackData[sizeOfSomeStack];
 
 	someStackPtr->stackData[sizeOfSomeStack] = POISON;
 
@@ -70,17 +78,24 @@ int StackDtor(Stack *someStackPtr){//деструктор стека
 
 	ASSERT_OK(someStackPtr);
 
-	memset(someStackPtr->stackData, POISON, someStackPtr->stackCapacity);
+	memset(someStackPtr->stackData, 0xF0, someStackPtr->stackCapacity);
 
 	free(someStackPtr->stackData);
-
-	someStackPtr = (Stack *)FREED_POINTER;// 13 - маленьное число, маркер уже освобожденных указателей 
-
-	//someStackPtr->stackSize = INT_MAX;//самое большое число size_t какое?
-	//someStackPtr->stackCapacity = 0;//норм????  a главное зачем....
+	
+	someStackPtr->stackSize = SIZE_T_MAX;//самое большое число size_t какое?
+	someStackPtr->stackCapacity = 0;//норм????  a главное зачем....
 
 	return NO_ERRORS;
 }
+
+/*
+void foo()
+{
+	Stack stk = {};
+	...
+	StackDtor (&stk);
+}
+*/
 
 //надо бы протестить как-то, а так норм
 int StackResize (Stack *someStackPtr){//управлет размером памяти, выделенной под стек
@@ -89,10 +104,10 @@ int StackResize (Stack *someStackPtr){//управлет размером пам
 
 	int sizeOfSomeStack     = someStackPtr->stackSize;
 	int capacityOfSomeStack = someStackPtr->stackCapacity;
-	int *dataOfSomeStack    = someStackPtr->stackData;
+	Type *dataOfSomeStack    = someStackPtr->stackData;
 
 	if (capacityOfSomeStack >= LARGE_VOLUME_CRITERION * sizeOfSomeStack){
-		int *reallocDataPtr = (int *)realloc(dataOfSomeStack, capacityOfSomeStack * NORMAL_DECREASE_COEFF);
+		Type *reallocDataPtr = (Type *)realloc(dataOfSomeStack, capacityOfSomeStack * NORMAL_DECREASE_COEFF);
 
 		if (reallocDataPtr == nullptr) {//будет тупо выводить это в отдельную функцию, так как придется проверять что она вернула
 			return LACK_OF_MEMORY;
@@ -107,7 +122,7 @@ int StackResize (Stack *someStackPtr){//управлет размером пам
 
 		if (sizeOfSomeStack > BIG_SIZE_OF_STACK) {
 
-			int *reallocDataPtr = (int *)realloc(dataOfSomeStack, capacityOfSomeStack * SMALL_INCREASE_COEFF);
+			Type *reallocDataPtr = (Type *)realloc(dataOfSomeStack, capacityOfSomeStack * SMALL_INCREASE_COEFF);
 
 			if (reallocDataPtr == nullptr) {
 				return LACK_OF_MEMORY;
@@ -118,7 +133,7 @@ int StackResize (Stack *someStackPtr){//управлет размером пам
 		}
 
 		else {
-			int *reallocDataPtr = (int *)realloc(dataOfSomeStack, capacityOfSomeStack * NORMAL_INCREASE_COEFF);
+			Type *reallocDataPtr = (Type *)realloc(dataOfSomeStack, capacityOfSomeStack * NORMAL_INCREASE_COEFF);
 
 			if (reallocDataPtr == nullptr) {
 				return LACK_OF_MEMORY;
@@ -135,7 +150,7 @@ int StackResize (Stack *someStackPtr){//управлет размером пам
 }
 
 //ну норм
-void StackPrint(const Stack someStack){//тут тоже assert_ok?
+void IntStackPrint(const Stack someStack){//тут тоже assert_ok?
 
 	PrintSeparator();
 	printf("Printing stack...\n\n");
@@ -179,7 +194,7 @@ int StackNotOK(const Stack *someStackPtr, const int line, const char *file, cons
 		return TWICE_CALLED_DTOR;
 	}
 
-	if (!someStackPtr->stackData){
+	if (!(someStackPtr->stackData)){
 		fprintf(logFile, "Указатель на данные стека равен нулю\n");
 		fclose(logFile);//это что, перед каждым return делать???
 		return STK_DATA_UNDEFINED;
@@ -198,7 +213,7 @@ int StackNotOK(const Stack *someStackPtr, const int line, const char *file, cons
 
 int StackDump_(const Stack *someStackPtr, const int line, const char *file, const char *function_name){
 //а здесь с режимом a или w лучше открыть??
-	FILE *FileDump = fopen("LifoDamp.html", "w");
+	FILE *FileDump = fopen("LifoDump.html", "w");
 
 	int statusStackNotOK = StackNotOK(someStackPtr, __LINE__, __FILE__, __FUNCTION__);
 
@@ -226,7 +241,7 @@ int StackDump_(const Stack *someStackPtr, const int line, const char *file, cons
 
 void PrintElement(const Stack *someStackPtr){
 
-FILE *FileDump = fopen("LifoDamp.html", "a");
+FILE *FileDump = fopen("LifoDump.html", "a");
 fprintf(FileDump, "<pre>");
 	for (int i = 0; i < someStackPtr->stackCapacity; i++){
 		if (i < someStackPtr->stackSize){
@@ -274,4 +289,17 @@ void MyMemcpy(void *newObject, const void *oldObject, size_t numberOfSymbols){
 
 	}
 		
+}
+
+Type StackTop(Stack someStack){
+
+	ASSERT_OK(&someStack);
+
+	size_t size = someStack.stackSize - 1;
+
+	Type valueFromTop = someStack.stackData[size];
+
+	ASSERT_OK(&someStack);
+
+	return valueFromTop;
 }
