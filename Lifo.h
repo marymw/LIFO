8d770e6497d4a175d3baaf12426ddd5ef55fc10d
marymw <71140577+marymw@users.noreply.github.com>
2021-10-05 17:46:23 +0300

    Update Lifo.h

--- Lifo.h
+++ Lifo.h
@@ -1,3 +1,6 @@
+#ifndef LIFO_H
+#define LIFO_H
+
 #include <string.h>
 
 #define DEBUG 1
@@ -16,7 +19,8 @@ enum Errors {
 	SIZE_LARGER_CAPACITY,//3
 	STK_DATA_UNDEFINED,	 //4
 	TWICE_CALLED_DTOR,   //5
-	CANARY_MISMATCH		 //6
+	CANARY_MISMATCH,     //6
+	HASH_MISMATCH		 //7
 };
 
 enum Constants{
@@ -28,39 +32,87 @@ enum Constants{
 	SMALL_INCREASE_COEFF	//1.1
 };
 
-typedef int Type;
+typedef int 				   Type;
 typedef unsigned long long int canary_t;
+typedef unsigned int 		   hash_t;
 
-extern const Type POISON        ;
-extern const int FREED_POINTER ;
-extern const size_t SIZE_T_MAX;
+extern const Type 					POISON;
+extern const Type 					CANARY_FOR_DATA;
+extern const int 					FREED_POINTER;
+extern const size_t 				SIZE_T_MAX;
 extern const unsigned long long int CANARY_CONST;
+
+#define NullPtrCheck(someStackPtr, logFile, string, nameOfError) do {							 \
+																	if (!someStackPtr){		     \
+																		fprintf(logFile, string);\
+																		fclose(logFile);		 \
+																		return(nameOfError);     \
+																	}	 						 \
+																 } while(0)
+
+
+#define FreedPtrCheck(someStackPtr, nameOfConst, logFile, string, nameOfError) do {							  \
+																	if(someStackPtr == (Stack *)nameOfConst){ \
+																		fprintf(logFile, string);			  \
+																		fclose(logFile);		 		      \
+																		return(nameOfError);				  \
+																	}	 				 					  \
+																		       } while(0)
+
+
+#define CanaryCheck(canary, canaryConst, logFile, string, nameOfError) do {									     \
+																			if (canary != canaryConst){ 		 \
+																				fprintf(logFile, string, canary);\
+																				fclose(logFile);				 \
+																				return nameOfError;			     \
+																			}									 \
+																		} while(0)
+#define HashCheck(hashValue, hashExpectedValue, logFile, string, nameOfError) do {							     					   \
+														if (hashValue != hashExpectedValue){ 					   \
+															fprintf(logFile, string, hashValue, hashExpectedValue);\
+															fclose(logFile);				 					   \
+															return nameOfError;			     					   \
+														}									 					   \
+																		     } while(0)
 
 
 struct Stack{
-	//void   *stackData;//указатель на начало данных
-	//size_t sizeOfTypeOfElements;//это вводит сам пользователь?
+	
 	canary_t canary1;
-	Type *stackData;
-	size_t stackSize;
-	size_t stackCapacity; 
+
+	Type  	*stackData;
+	size_t   stackSize;
+	size_t   stackCapacity; 
+
+	hash_t   hash;
+
 	canary_t canary2;
 };
 
-#define ASSERT_OK(someStackPtr) if((StackNotOK(someStackPtr, __LINE__, __FILE__, __FUNCTION__))){StackDump(someStackPtr);assert((!"Stack OK"));}
+#define ASSERT_OK(someStackPtr) do {																 \
+									if((StackNotOK(someStackPtr, __LINE__, __FILE__, __FUNCTION__))){\
+ 										StackDump(someStackPtr);									 \
+ 										assert((!"Stack OK"));}										 \
+						        } while (0)																													
 
-#define StackDump(someStackPtr) {StackDump_(someStackPtr, __LINE__, __FILE__, __FUNCTION__);}
 
-int  StackCtor     (Stack *firstStackPtr);
-int  StackDtor     (Stack *firstStackPtr);
-int  StackResize   (Stack *someStackPtr);
-Type StackTop      (Stack someStack);
-int  StackPush     (Stack *firstStackPtr, Type value);
-Type StackPop      (Stack *someStackPtr, int *statusStackPop = NULL);
-int  StackDump_    (const Stack *someStackPtr, const int line, const char *file, const char *function_name);
-int  StackNotOK    (const Stack *someStackPtr, const int line, const char *file, const char *function_name);
-void IntStackPrint (Stack someStack);
-void PrintElement  (const Stack *someStackPtr);
-void PrintSeparator();
-void MyMemcpy      (void *newObject, const void *oldObject, size_t numberOfSymbols);
+#define StackDump(someStackPtr) do { 																\
+									StackDump_(someStackPtr, __LINE__, __FILE__, __FUNCTION__);		\
+								} while (0)															
 
+
+int    StackDtor     (Stack *someStackPtr);
+int    StackCtor     (Stack *someStackPtr, size_t someStackCapacity);
+
+Type   StackTop      (Stack someStack);
+int    StackPush     (Stack *firstStackPtr, Type value);
+Type   StackPop      (Stack *someStackPtr, int *statusStackPop = NULL);
+int    StackDump_    (const Stack *someStackPtr, const int line, const char *file, const char *function_name);
+int    StackNotOK    (const Stack *someStackPtr, const int line, const char *file, const char *function_name);
+void   IntStackPrint (Stack someStack);
+void   PrintElement  (const Stack *someStackPtr);
+void   PrintSeparator();
+void   MyMemcpy      (void *newObject, const void *oldObject, size_t numberOfSymbols);
+hash_t MyHashRot13   (const char * str);
+
+#endif LIFO_H
