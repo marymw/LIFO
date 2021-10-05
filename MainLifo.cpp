3fd39935088a349a1ff57fc85612ba6aa314f9b2
marymw <71140577+marymw@users.noreply.github.com>
2021-10-06 00:26:15 +0300

    Update MainLifo.cpp

--- MainLifo.cpp
+++ MainLifo.cpp
@@ -4,12 +4,12 @@
 
 
 int main() {
-	Stack firstStack = {}; //типа инициализация
+	Stack firstStack = {}; 
 
-	int statusStackCtor = StackCtor(&firstStack, 10); //конструктор стека, создает стек, возвращает ошибку
+	int statusStackCtor = StackCtor(&firstStack, 10); 
 	assert(statusStackCtor == 0);
 
-	StackDump(&firstStack);
+	//StackDump(&firstStack);
 
 	int statusStackPush = StackPush(&firstStack, 10);//кладет что-то в конец стека
 	assert(statusStackPush == 0);
@@ -20,20 +20,14 @@ int main() {
 	statusStackPush = StackPush(&firstStack, 20);//кладет что-то в конец стека
 	assert(statusStackPush == 0);
 
-	int statusStackPop = 0;
-
 	StackDump(&firstStack);
+	int statusStackPop = 0;
 
 	int valueFromPop1 = StackPop(&firstStack, &statusStackPop); //вытаскивает один элемент из стека
 	assert(statusStackPop == 0);
 
-	int valueFromPop2 = StackPop(&firstStack); //вытаскивает один элемент из стека
-	assert(statusStackPop == 0);
-
 	int statusStackDtor = StackDtor(&firstStack);//деструктор стека
 	assert(statusStackDtor == 0);
-
-	//StackDtor(&firstStack); //пока не работает проверка
 
 	return 0;
 }
