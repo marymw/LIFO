7f5e502d13785d61c13cc9a8c33d4e2c8947fc38
marymw <71140577+marymw@users.noreply.github.com>
2021-10-06 20:41:28 +0300

    Update Lifo.h

--- Lifo.h
+++ Lifo.h
@@ -20,74 +20,74 @@
 #endif
 
 enum Errors {
-	NO_ERRORS,      	 //0
-	STK_UNDEFINED,       //1
-	LACK_OF_MEMORY, 	 //2
-	SIZE_LARGER_CAPACITY,//3
-	STK_DATA_UNDEFINED,	 //4
-	TWICE_CALLED_DTOR,   //5
-	CANARY_MISMATCH,     //6
-	HASH_MISMATCH,		 //7
-	ERROR_IN_OPEN_LOG,	 //8
-	ERROR_IN_OPEN_DUMP	 //9
+	NO_ERRORS,            //0
+	STK_UNDEFINED,        //1
+	LACK_OF_MEMORY,       //2
+	SIZE_LARGER_CAPACITY, //3
+	STK_DATA_UNDEFINED,   //4
+	TWICE_CALLED_DTOR,    //5
+	CANARY_MISMATCH,      //6
+	HASH_MISMATCH,	      //7
+	ERROR_IN_OPEN_LOG,    //8
+	ERROR_IN_OPEN_DUMP    //9
 };
 
 enum Constants{
 	LARGE_VOLUME_CRITERION = 8,
 	SMALL_VOLUME_CRITERION = 2,
-	NORMAL_DECREASE_COEFF = 2,  
-	BIG_SIZE_OF_STACK = 100,		
-	NORMAL_INCREASE_COEFF = 3,  
-	SMALL_INCREASE_COEFF = 2	
+	NORMAL_DECREASE_COEFF  = 2,  
+	BIG_SIZE_OF_STACK      = 100,		
+	NORMAL_INCREASE_COEFF  = 3,  
+	SMALL_INCREASE_COEFF   = 2	
 };
 
-typedef double 				   Type;
+typedef double 		       Type;
 typedef unsigned long long int canary_t;
-typedef unsigned int 		   hash_t;
+typedef unsigned int 	       hash_t;
 
-extern const Type 					POISON;
-extern const Type 					CANARY_FOR_DATA;
-extern const int 					FREED_POINTER;
-extern const size_t 				SIZE_T_MAX;
+extern const Type 		    POISON;
+extern const Type 		    CANARY_FOR_DATA;
+extern const int 		    FREED_POINTER;
+extern const size_t 		    SIZE_T_MAX;
 extern const unsigned long long int CANARY_CONST;
 
-#define NullPtrCheck(someStackPtr, logFile, string, nameOfError) do {							 \
-																	if (!someStackPtr){		     \
-																		fprintf(logFile, string);\
-																		return(nameOfError);     \
-																	}	 						 \
-																 } while(0)
+#define NullPtrCheck(someStackPtr, logFile, string, nameOfError) do {			                 \
+									if (!someStackPtr){		 \
+										fprintf(logFile, string);\
+										return(nameOfError);     \
+									}	 			 \
+								  } while(0)
 
 
-#define FreedPtrCheck(someStackPtr, nameOfConst, logFile, string, nameOfError) do {							  \
-																	if(someStackPtr == (Stack *)nameOfConst){ \
-																		fprintf(logFile, string);			  \
-																		return(nameOfError);				  \
-																	}	 				 					  \
-																		       } while(0)
+#define FreedPtrCheck(someStackPtr, nameOfConst, logFile, string, nameOfError) do {						  \
+											if(someStackPtr == (Stack *)nameOfConst){ \
+												fprintf(logFile, string);	  \
+												return(nameOfError);		  \
+											}	 				  \
+									       } while(0)
 
 
-#define CanaryCheck(canary, canaryConst, logFile, string, nameOfError) do {									     \
-																			if (canary != canaryConst){ 		 \
-																				fprintf(logFile, string, canary);\
-																				return nameOfError;			     \
-																			}									 \
-																		} while(0)
+#define CanaryCheck(canary, canaryConst, logFile, string, nameOfError) do {			   		         \
+										if (canary != canaryConst){ 		 \
+											fprintf(logFile, string, canary);\
+											return nameOfError;              \
+										}					 \
+									} while(0)
 
-#define HashCheck(hashValue, hashExpectedValue, logFile, string, nameOfError) do {								   \
-														if (hashValue != hashExpectedValue){ 					   \
-															fprintf(logFile, string, hashValue, hashExpectedValue);\
-															return nameOfError;			     					   \
-														}									 					   \
-																		     } while(0)
+#define HashCheck(hashValue, hashExpectedValue, logFile, string, nameOfError) do {				    				       \
+											if (hashValue != hashExpectedValue){ 			       \
+												fprintf(logFile, string, hashValue, hashExpectedValue);\
+												return nameOfError;				       \
+											}			 				       \
+									      } while(0)
 
 
 #define CompOsSizeAndCapacityCheck(size, capacity, logFile, string, nameOfError) do {								\
-																		    if(size > capacity){					\
-																				fprintf(logFile, string);			\
-																				return nameOfError;					\
-																			}										\
-																		 }while(0)
+											 if(size > capacity){					\
+											 	fprintf(logFile, string);			\
+												return nameOfError;				\
+											}			   				\
+										 }while(0)
 
 
 struct Stack{
@@ -103,16 +103,17 @@ struct Stack{
 	canary_t canary2;
 };
 
-#define ASSERT_OK(someStackPtr) do {																 \
-									if((StackNotOK(someStackPtr, __LINE__, __FILE__, __FUNCTION__))){\
- 										StackDump(someStackPtr);									 \
- 										assert((!"Stack OK"));}										 \
-						        } while (0)																													
+#define ASSERT_OK(someStackPtr) do {									 \
+					if((StackNotOK(someStackPtr, __LINE__, __FILE__, __FUNCTION__))){\
+ 						StackDump(someStackPtr);				 \
+ 						assert((!"Stack OK"));					 \
+					}								 \
+				 } while (0)																				
 
 
-#define StackDump(someStackPtr) do { 																\
-									StackDump_(someStackPtr, __LINE__, __FILE__, __FUNCTION__);		\
-								} while (0)															
+#define StackDump(someStackPtr) do { 									\
+					StackDump_(someStackPtr, __LINE__, __FILE__, __FUNCTION__);	\
+				} while (0)															
 
 
 int    StackDtor     (Stack *someStackPtr);
